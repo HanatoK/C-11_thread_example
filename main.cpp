0700ce80f175567fbcbc71f6314915947f832337
hanatok <summersnow9403@gmail.com>
2021-05-05 20:09:22 -0500

    Revert "Shared conditional variable."
    This reverts commit 746f5b506b362e1499e019e6df051176611b69fd.

--- main.cpp
+++ main.cpp
@@ -6,23 +6,21 @@
 #include <mutex>
 #include <thread>
 #include <vector>
-#include <numeric>
 
 class MultiThreadCalc {
 private:
   std::vector<double> mData;
   std::vector<double> mReference;
   std::vector<std::thread> mThreads;
-  std::condition_variable mSharedCond;
-  std::mutex mSharedMutex;
+  std::vector<std::condition_variable> mConds;
+  std::vector<std::mutex> mMutexes;
   std::vector<int> mTaskStates;
   size_t mNumBlocks;
   bool mShutdown;
   bool mFirstTime;
   void worker(size_t thread_index) {
     while (mTaskStates[thread_index] == 0 && !mShutdown) {
-      std::mutex m;
-      std::unique_lock<std::mutex> lk(m);
+      std::unique_lock<std::mutex> lk(mMutexes[thread_index]);
       const size_t mStride = mThreads.size();
       for (size_t block_index = 0; block_index < mNumBlocks; ++block_index) {
         const size_t data_index = block_index * mStride + thread_index;
@@ -32,8 +30,8 @@ private:
         }
       }
       mTaskStates[thread_index] = 1;
-      mSharedCond.notify_all();
-      mSharedCond.wait(lk, [this, thread_index]() {
+      mConds[thread_index].notify_one();
+      mConds[thread_index].wait(lk, [this, thread_index]() {
         return mTaskStates[thread_index] == 0;
       });
     }
@@ -41,8 +39,9 @@ private:
 public:
   MultiThreadCalc(size_t numData,
                   size_t numThreads = std::thread::hardware_concurrency())
-      : mData(numData, 0), mThreads(numThreads), mTaskStates(numThreads),
-        mShutdown(false), mFirstTime(true) {
+      : mData(numData, 0), mThreads(numThreads), mConds(numThreads),
+        mMutexes(numThreads), mTaskStates(numThreads), mShutdown(false),
+        mFirstTime(true) {
     mNumBlocks = numData / numThreads + 1;
     std::cout << "mNumBlocks = " << mNumBlocks << std::endl;
     std::vector<double> mReference(mData.size());
@@ -59,8 +58,12 @@ public:
   ~MultiThreadCalc() {
     std::cout << "~MultiThreadCalc()\n";
     mShutdown = true;
-    std::fill(mTaskStates.begin(), mTaskStates.end(), 0);
-    mSharedCond.notify_all();
+    for (size_t i = 0; i < mConds.size(); ++i) {
+      std::unique_lock<std::mutex> lk(mMutexes[i]);
+      lk.unlock();
+      mTaskStates[i] = 0;
+      mConds[i].notify_one();
+    }
     for (size_t i = 0; i < mThreads.size(); ++i) {
       if (mThreads[i].joinable()) mThreads[i].join();
     }
@@ -68,19 +71,19 @@ public:
   void run() {
     for (int i = 0; i < 10; ++i) {
       std::fill(mData.begin(), mData.end(), 0.0);
-      for (size_t i = 0; i < mTaskStates.size(); ++i) {
+      for (size_t i = 0; i < mConds.size(); ++i) {
         mTaskStates[i] = 0;
         if (mFirstTime) mThreads[i] = std::thread(&MultiThreadCalc::worker, this, i);
+        if (!mFirstTime) mConds[i].notify_one();
       }
       if (mFirstTime) {
         mFirstTime = false;
-      } else {
-        mSharedCond.notify_all();
       }
       const auto start = std::chrono::steady_clock::now();
-      std::unique_lock<std::mutex> lk(mSharedMutex);
-      mSharedCond.wait(lk, [this](){
-        return static_cast<size_t>(std::accumulate(mTaskStates.begin(), mTaskStates.end(), 0)) == mTaskStates.size();});
+      for (size_t i = 0; i < mConds.size(); ++i) {
+        std::unique_lock<std::mutex> lk(mMutexes[i]);
+        mConds[i].wait(lk, [this, i]() { return mTaskStates[i] == 1; });
+      }
       const auto end = std::chrono::steady_clock::now();
       double error = 0;
       for (size_t i = 0; i < mReference.size(); ++i) {
@@ -94,7 +97,7 @@ public:
 };
 
 int main() {
-  MultiThreadCalc obj(25497035);
+  MultiThreadCalc obj(25497563);
   obj.run();
   return 0;
 }
